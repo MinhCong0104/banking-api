a4718746aaddb802151e670ecb0c3a4cf64e3b8c
congtm <congtm.bhsoft@gmail.com>
2024-03-28 18:05:16 +0700

    Update auth gg sheet

--- app/interfaces/rest/api_v1/endpoints/gg_shet.py
+++ app/interfaces/rest/api_v1/endpoints/gg_shet.py
@@ -3,10 +3,12 @@ from fastapi import APIRouter, Body, Depends, Path, Query, UploadFile, File
 from typing import Annotated, Union, Dict, List
 from app.shared.decorator import response_decorator
 from app.config import settings
-import google.auth
+
+from google import auth
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 
 router = APIRouter()
@@ -22,37 +24,45 @@ Ví dụ cần đóng 120k mới đủ -> Notice: thiếu 20k
 Chỉ cần đóng 80k -> Thêm 20k vào quỹ
 """
 
-def batch_update_values(
-    spreadsheet_id, range_name, value_input_option, _values
-):
-  """
-  Creates the batch_update the user has access to.
-  Load pre-authorized user credentials from the environment.
-  for guides on implementing OAuth2 for the application.
-  """
-  creds, _ = google.auth.default()
-  # pylint: disable=maybe-no-member
-  try:
-    service = build("sheets", "v4", credentials=creds)
+def write_value(spread_name, sheet_name):
+    credentials = ServiceAccountCredentials.from_json_keyfile_name("app/infra/service/annular-form-362316-6658c0d75433.json")
+    client = gspread.authorize(credentials)
+    spreadsheet = client.open(spread_name)
+    worksheet = spreadsheet.worksheet(sheet_name)
+    worksheet.update('A1', 'Giá trị mới')
 
-    values = [
-        [],
-    ]
-    data = [
-        {"range": range_name, "values": values},
-    ]
-    body = {"valueInputOption": value_input_option, "data": data}
-    result = (
-        service.spreadsheets()
-        .values()
-        .batchUpdate(spreadsheetId=spreadsheet_id, body=body)
-        .execute()
-    )
-    print(f"{(result.get('totalUpdatedCells'))} cells updated.")
-    return result
-  except HttpError as error:
-    print(f"An error occurred: {error}")
-    return error
+
+# def batch_update_values(
+#     spreadsheet_id, range_name, value_input_option, _values
+# ):
+#   """
+#   Creates the batch_update the user has access to.
+#   Load pre-authorized user credentials from the environment.
+#   for guides on implementing OAuth2 for the application.
+#   """
+#   creds, _ = google.auth.default()
+#   # pylint: disable=maybe-no-member
+#   try:
+#     service = build("sheets", "v4", credentials=creds)
+#
+#     values = [
+#         [],
+#     ]
+#     data = [
+#         {"range": range_name, "values": values},
+#     ]
+#     body = {"valueInputOption": value_input_option, "data": data}
+#     result = (
+#         service.spreadsheets()
+#         .values()
+#         .batchUpdate(spreadsheetId=spreadsheet_id, body=body)
+#         .execute()
+#     )
+#     print(f"{(result.get('totalUpdatedCells'))} cells updated.")
+#     return result
+#   except HttpError as error:
+#     print(f"An error occurred: {error}")
+#     return error
 
 
 @router.get("")
@@ -88,10 +98,5 @@ def write_data(
 
 
 
-    batch_update_values(
-        settings.GG_SHEET_ID,
-        f"{range}!A1",
-        "USER_ENTERED",
-        [["X"]],
-    )
+    write_value('test', 'sheet_test')
     return {"Response": "OK"}
